c7bf8e36cb407675065a53243c57efc1066d97d0
Rytis Ilciukas <zibalas@gmail.com>
2016-09-12 10:05:08 +0200

    re-arrange parameters

--- bin/run.sh
+++ bin/run.sh
@@ -12,7 +12,7 @@ if [[ ! -d $DIR ]]; then
 	MSG "Downloading podcastgen..."
 	wget -O /tmp/podcastgen.zip https://sourceforge.net/projects/podcastgen/files/latest/download
 	[[ $? -eq 0 ]] || { ERR "Failed to download podcastgen, aborting."; exit 1; }
-	unzip -o -f -d -v /var/www /tmp/podcastgen.zip
+	unzip -v -o -f -d /var/www /tmp/podcastgen.zip
 	[[ -d $DIR ]] || { ERR "Directory $DIR does not exist, aborting."; exit 1; }
 	chown -R www-data:www-data /var/www/
 fi
