9d36700b881d117560e026dac602864844f7e80f
Scott Humphries <sscotth@users.noreply.github.com>
2018-01-12 20:03:08 -0600

    Use github instead of sourceforge

--- bin/run.sh
+++ bin/run.sh
@@ -9,10 +9,8 @@ CHECK_BIN "uwsgi"
 DIR=/var/www/PodcastGenerator-master
 
 if [[ ! -d $DIR ]]; then
-	MSG "Downloading podcastgen..."
-	wget -O /tmp/podcastgen.zip https://sourceforge.net/projects/podcastgen/files/latest/download
-	[[ $? -eq 0 ]] || { ERR "Failed to download podcastgen, aborting."; exit 1; }
-	unzip -o -d /var/www /tmp/podcastgen.zip
+	# MSG "Downloading podcastgen..."
+	git clone https://github.com/albertobeta/PodcastGenerator $DIR
 	ls -la /var/www
 	[[ -d $DIR ]] || { ERR "Directory $DIR does not exist, aborting."; exit 1; }
 	chown -R www-data:www-data /var/www/
